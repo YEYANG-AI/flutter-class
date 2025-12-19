86948c16a66a919e1a70d2b768a7677ee68b3e03
YessYang <brokenheartofficials@gmail.com>
2025-12-19 09:46:23 +0700

    add

--- appnews/lib/main.dart
+++ appnews/lib/main.dart
@@ -12,15 +12,21 @@ class myapp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         appBarTheme: AppBarTheme(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(20),
+              bottomRight: Radius.circular(20),
+            ),
+            side: BorderSide(color: Colors.green, width: 2),
+          ),
           centerTitle: true,
-          backgroundColor: Colors.green.shade900,
           titleTextStyle: TextStyle(
-            color: Colors.white,
+            color: Colors.green,
             fontSize: 20,
             fontFamily: 'NotoSansLao',
             fontWeight: FontWeight.bold,
           ),
-          iconTheme: IconThemeData(color: Colors.white),
+          iconTheme: IconThemeData(color: Colors.green),
         ),
       ),
       home: HomePage(),
