b23b1f3b9c42c15d310ff4b02b933254d74ca162
YessYang <brokenheartofficials@gmail.com>
2025-12-19 10:00:14 +0700

    add

--- appnews/lib/signUp/signUp_page.dart
+++ appnews/lib/signUp/signUp_page.dart
@@ -21,50 +21,76 @@ class _SignupPageState extends State<SignupPage> {
         ),
         title: Text('ຈັດການຂໍ້ມູນຜູ້ໃຊ້ງານ'),
       ),
-      body: Container(
-        margin: EdgeInsets.all(20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              decoration: InputDecoration(
-                fillColor: Colors.white,
-                filled: true,
-                prefixIcon: Icon(
-                  Icons.person_add_alt_sharp,
-                  color: Colors.green,
+      body: SizedBox(
+        child: Padding(
+          padding: const EdgeInsets.all(15),
+          child: Center(
+            child: Card(
+              color: Colors.grey.shade500,
+              elevation: 10,
+              shadowColor: Colors.white,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 10,
+                  vertical: 20,
                 ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(
+                        fillColor: Colors.white,
+                        filled: true,
+                        prefixIcon: Icon(
+                          Icons.person_add_alt_sharp,
+                          color: Colors.green,
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        labelText: 'ຊື່ຜູ້ໃຊ້',
+                        labelStyle: TextStyle(color: Colors.green),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    TextField(
+                      decoration: InputDecoration(
+                        fillColor: Colors.white,
+                        filled: true,
+                        prefixIcon: Icon(Icons.lock, color: Colors.green),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        labelText: 'ລະຫັດຜ່ານ',
+                        labelStyle: TextStyle(color: Colors.green),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Container(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 20,
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.white, width: 2),
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Text(
+                        'ບັນທຶກ',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-                labelText: 'ຊື່ຜູ້ໃຊ້',
               ),
             ),
-            SizedBox(height: 20),
-            TextField(
-              decoration: InputDecoration(
-                fillColor: Colors.white,
-                filled: true,
-                prefixIcon: Icon(Icons.lock, color: Colors.blue),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                labelText: 'ລະຫັດຜ່ານ',
-              ),
-            ),
-            SizedBox(height: 20),
-            Container(
-              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-              decoration: BoxDecoration(
-                color: Colors.red,
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: Text(
-                'ບັນທຶກ',
-                style: TextStyle(color: Colors.white, fontSize: 16),
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
